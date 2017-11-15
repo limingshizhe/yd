b5416946ad369cd17f92ae341c1d470e984739fe
limingshizhe <1137846162@qq.com>
2017-11-15 18:07:41 +0800

    sdf

--- js/b.js
+++ js/b.js
@@ -1,6 +1,8 @@
 $(function(){
-	$('.wjm').click(function(){
-		console.log($('#username').value)
+	$('#dl').click(function(){
+		alert("不要点了，目前出了一点小故障，等我修护好了再告诉你")
+		
+		window.location.href="https://limingshizhe.github.io/yd/b";
 	})
 		
 	
