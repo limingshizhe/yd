7a913ddbae8041bd300265e139f6287f288c1355
limingshizhe <1137846162@qq.com>
2017-11-16 19:32:36 +0800

    koipjf

--- js/c.js
+++ js/c.js
@@ -8,15 +8,15 @@ $(function(){
 	
 	function ov(){
 		$('#uls').children().eq(i-1).animate({left:-50+'%'},1000);
-		$('#uls').children().eq(i).css('left','50%').animate({left:0},1000);
+		$('#uls').children().eq(i).css('left','49%').animate({left:0},1000);
 		$('#ols').children().eq(i).addClass('lisl').siblings('li').removeClass('lisl')
 	}
 	function ou(s){
 	    if(s==i){
 	    	return;
 	    }
-	    $('#uls').children().eq(i).animate({left:-50+'%'},1000);
-		$('#uls').children().eq(s).css('left','50%').animate({left:0},1000);
+	    $('#uls').children().eq(i).animate({left:50+'%'},1000);
+		$('#uls').children().eq(s).css('left','-49%').animate({left:0},1000);
 		$('#ols').children().eq(s).addClass('lisl').siblings('li').removeClass('lisl')
 	    i=s;
 	    
